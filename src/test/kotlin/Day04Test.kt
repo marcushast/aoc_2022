b6c669133e62db00697abc37a9445ad352648d43
Marcus Hast <marcushast@gmail.com>
2022-12-06 18:34:10 +0100

    Solution for Day04

---
+++ src/test/kotlin/Day04Test.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+
+internal class Day04Test {
+
+    @Test
+    fun Problem1_1() {
+        Day04.task1_test()
+    }
+
+    @Test
+    fun Problem1_2() {
+        Day04.task1_real()
+    }
+
+    @Test
+    fun Problem2_1() {
+        Day04.task2_test()
+    }
+
+    @Test
+    fun Problem2_2() {
+        Day04.task2_real()
+    }
+
+//    @Test
+//    fun Problem1_2() {
+//        Day01.work(Day01.process(Day01.data2))
+//    }
+//
+//    @Test
+//    fun Problem2_2() {
+//        Day01.work2(Day01.process(Day01.data2))
+//    }
+}
